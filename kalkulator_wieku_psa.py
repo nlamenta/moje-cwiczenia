9a32e94a3f429f765068a8a03bab9a41cce1dd8d
Natalia Lamenta <83751@student.ue.poznan.pl>
2024-03-26 09:08:23 +0100

    Initial commit

---
+++ kalkulator_wieku_psa.py
@@ -0,0 +1,7 @@
+# Zadanie: Kalkulator wieku psa
+# Napisz prosty program, który obliczy wiek psa w psich latach na podstawie jego wieku w ludzkich latach. Zwykle mówi się, że pierwsze dwa lata życia psa odpowiadają około 10,5 ludzkiego roku, a każdy kolejny rok psa odpowiada około 4 ludzkich latom.
+#
+# Wskazówki:
+# Poproś użytkownika o podanie wieku swojego psa w ludzkich latach.
+# Użyj podanych proporcji, aby obliczyć wiek psa w psich latach.
+# Wydrukuj wynik.
