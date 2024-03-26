98cd870c940b8ef875246c4c552e287e4157e20b
Natalia Lamenta <83751@student.ue.poznan.pl>
2024-03-26 17:52:05 +0100

    skończony kalkulator

--- kalkulator_wieku_psa.py
+++ kalkulator_wieku_psa.py
@@ -5,3 +5,15 @@
 # Poproś użytkownika o podanie wieku swojego psa w ludzkich latach.
 # Użyj podanych proporcji, aby obliczyć wiek psa w psich latach.
 # Wydrukuj wynik.
+
+import math as math
+
+def wiek_psa(wiek):
+    wiek_psa = 16 * math.log(wiek) + 31
+    return wiek_psa
+
+wejscie = int(input("Podaj wiek swojego psa w latach ludzkich: "))
+wiek = wiek_psa(wejscie)
+
+print("Wiek twojego psich latach:", round(wiek, 0))
+
