035195355ac64d6cc3a45c33d76355c415f94f81
Natalia Lamenta <83751@student.ue.poznan.pl>
2024-03-26 17:53:55 +0100

    Initial commit

---
+++ analiza_tekstu.py
@@ -0,0 +1,13 @@
+# Napisz program, który analizuje tekst wprowadzony przez użytkownika i wykonuje następujące czynności:
+#
+# Oblicza liczbę słów w tekście.
+# Oblicza liczbę zdań w tekście.
+# Oblicza średnią długość słowa w tekście (w znakach).
+# Wypisuje najczęściej występujące słowo w tekście.
+# Wypisuje najczęściej występujący znak interpunkcyjny (., !, ?) oraz jego liczbę wystąpień.
+# Wskazówki:
+# Możesz użyć funkcji split() do podzielenia tekstu na słowa i funkcji len() do obliczenia liczby elementów w liście słów.
+# Zdania często kończą się znakami interpunkcyjnymi, takimi jak ".", "!" lub "?".
+# Aby obliczyć średnią długość słowa, możesz zsumować długość wszystkich słów i podzielić przez liczbę słów.
+# Możesz użyć słownika do zliczenia wystąpień poszczególnych słów i znaków interpunkcyjnych.
+# Po zliczeniu wszystkich słów i znaków interpunkcyjnych, możesz wybrać te, które występują najczęściej.
